7f9eaa4f11e96573498ea34ba8ef25a225ab9f15
unknown <Administrator@UK-L13M5PQ2.groupinfra.com>
2020-11-30 23:45:33 +0000

    Modified the sout statement

--- src/io/epreuveiot/revison/scm/GitIntegration.java
+++ src/io/epreuveiot/revison/scm/GitIntegration.java
@@ -2,6 +2,6 @@ package io.epreuveiot.revison.scm;
 
 public class GitIntegration {
     public static void main(String[] args) {
-        System.out.println("Connecting to GIT");
+        System.out.println("Integrating with GITHub");
     }
 }
